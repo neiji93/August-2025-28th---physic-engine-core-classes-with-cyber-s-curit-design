ed77189157e9375476b4767ec503cbc174fedb96
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-08-28 16:23:08 +0200

    Create Nablaonrigidbodies.h

---
+++ Nablaonrigidbodies.h
@@ -0,0 +1,6 @@
+//the nabla cannot be runned as a modelling term.
+//then, this is the best starting point to cyber defense of physic engines.
+//to get in touch, one prefer to add reflexions as programming design.
+
+class nonnablarigidbpfiesgettermovementequationo'cpusafe
+{}

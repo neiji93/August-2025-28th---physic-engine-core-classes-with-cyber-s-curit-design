169e42cd321a3229c47727873189d22946d1d4ab
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-08-28 16:01:48 +0200

    Create CoreModellingDynamicmathematicsOnRigkdBodied.h

---
+++ CoreModellingDynamicmathematicsOnRigkdBodied.h
@@ -0,0 +1,7 @@
+//the physics are highly suantified. to ensuire code decurity,
+//we better have to ude brandtl model. so we can use cpp reflections immediately here.
+//the spped wont be decreased compared to native code. so if prevent jsi' fpga too
+//we can have an ipen world physics here.
+//but the tiny objectd cannot be registered because of the lack
+//of reflective la'gages to.hzndle coppanies scripti'g langages for video game devloppment.
+//so the modelling process is just not mathematically ill defined.
